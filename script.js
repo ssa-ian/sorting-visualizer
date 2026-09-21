5d40af5c28935afa32a7eb89e67f06fbc13a4a17
sebastian.r.tedjo <sebastian.r.tedjo@accenture.com>
2026-09-21 09:53:55 +0200

    forgot what i updated

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const numbers = []
-const ol = []
+let run = true
+let stoppedRunning = false;
 
-for (let i = 0; i < 100; i++){
-    ol.push(i+1)
+const createNumbers = function(length){
+    const ar = []
+    const nr = []
+    for (let i = 0; i < length; i++){
+        ar.push(i+1)
+    }
+    for (let i = 0; i < length; i++){
+        nr.push(ar.splice(Math.floor(Math.random() * ar.length), 1)[0])
+    }
+    return nr;
 }
+
+
+
 
 function sleep(ms){
     return new Promise(resolve => {
@@ -11,19 +22,14 @@ function sleep(ms){
     })
 }
 
-const initLen = ol.length
-
-for (let i = 0; i < initLen; i ++){
-    numbers[i] = ol.splice(Math.floor(Math.random() * ol.length), 1)[0]
-}
 
 const columnContainer = document.getElementById("columnsContainer")
 
-function visualizeArray (array, id = -1){
+function visualizeArray (array, greenID = -1, redID = -1){
     columnContainer.replaceChildren()
 
-    array.forEach(i => {
-        const newDiv = document.createElement("div")
+    for(let i = 0; i<array.length; i++){
+    const newDiv = document.createElement("div")
 
     newDiv.style.display = "flex"
     newDiv.style.flexDirection = "column"
@@ -32,41 +38,66 @@ function visualizeArray (array, id = -1){
 
     const tallBar = document.createElement("div")
 
-    tallBar.style.backgroundColor = (i == id)?"#12FC00" : "black"
-    tallBar.style.flex = i / Math.max(...array)
+    tallBar.style.backgroundColor = (i == greenID)?"#12FC00" : (i == redID) ? "red" : "black"
+    tallBar.style.flex = array[i] / Math.max(...array)
 
     newDiv.appendChild(tallBar)
 
     columnContainer.appendChild(newDiv)
-    });
+    };
 }
 
-async function bubbleSort(numbers){
+let arrayToSort = createNumbers(100)
+visualizeArray(arrayToSort)
+
+async function bubbleSort(numbers, waitTime){
     let sorted = false;
     let unsortedLength = numbers.length
 
     while(!sorted){
+    if(!run){
+        break
+    }
     sorted = true
     for(let i = 0; i< unsortedLength - 1; i++){
+    if(!run){
+        break
+    }   
     if(numbers[i] > numbers[i+1]){
         sorted = false
         const temp = numbers[i]
         numbers[i] = numbers[i+1]
         numbers[i+1] = temp
-        visualizeArray(numbers, i)
-        await sleep(0.01)
-    }}
+        visualizeArray(numbers, i+1)
+    }else{
+        visualizeArray(numbers,-1, i+1)
+    }
+
+await sleep(waitTime)
+}
     unsortedLength--
 }
+
 visualizeArray(numbers)
 }
-
-
-visualizeArray(numbers)
 
 document.getElementById("startBtn").onclick = () => btnClick()
 
 function btnClick(){
-    bubbleSort(numbers)
+    run = !run
+    document.getElementById("startBtn").textContent = (!run) ? "Start" : "Stop" 
+    let speed = 0.1
+    try{
+        speed = document.getElementById("speed").value 
+    }catch(error){
+        alert("Problem with getting speed")
+    }
+    bubbleSort(arrayToSort, speed)
+}
+
+document.getElementById("generateBtn").onclick = () => {
+    run = false
+    arrayToSort = createNumbers(parseInt(document.getElementById("length").value))
+    visualizeArray(arrayToSort)
 }
 
